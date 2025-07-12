bd4805f3069d2589f84617e4a8d96351d08225b9
AtharvaKulkarni07 <kulkarniatharva206@gmail.com>
2025-07-12 15:16:24 +0530

    Added: Readme & google auth

---
+++ README.md
@@ -0,0 +1,10 @@
+# StackIt – A Minimal Q&A Forum Platform
+
+## Problem statement
+> StackIt is a minimal question-and-answer platform that supports collaborative learning and structured knowledge sharing. It’s designed to be simple, user- friendly, and focused on the core experience of asking and answering questions within a community.
+
+## Team Members
+* Atharva Kulkarni
+* Durgesh Shukla
+* Dhruv Yadav 
+* Sanvi Tripathi 
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,17 @@
-import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/auth';
+// app/api/auth/[...nextauth]/route.ts
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
